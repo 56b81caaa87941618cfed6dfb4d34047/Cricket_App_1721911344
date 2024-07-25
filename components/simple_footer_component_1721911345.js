12736aab9e8d5f67e7da3afac9ba749920fa2ce5
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-25 13:25:24 +0000

    Code changes merged into simple_footer_component_1721911345.js successfully

--- components/simple_footer_component_1721911345.js
+++ components/simple_footer_component_1721911345.js
@@ -2,13 +2,13 @@
 */
 Vue.component("simple_footer_component_1721911345", {
     template: `
-    <footer id="footer-section" style="height: 350px" class="flex-1 bg-white dark:bg-gray-800">
+    <footer id="footer-section" style="height: 430px"  class="flex-1 bg-white dark:bg-gray-800">
             <div id="footer-container" class="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
                 <hr id="footer-divider" class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
                 <div id="footer-content" class="text-center">
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
-                            <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
+                            <img id="footer-logo" src="https://makeinfinite-mentat-test.azurewebsites.net/get_image/Cricket_App_1721911344/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
                             CricketMate - Your Ultimate Cricket Companion
                         </a>
                     </div>
